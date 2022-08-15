cd4ad7da884ab2cba9fd419f6af063bf53f138c1
novastosha <galliigotu@gmail.com>
2022-08-15 23:45:37 +0100

    Made custom actor methods not abstract

--- src/main/java/net/zoda/api/command/wrapper/CommandManager.java
+++ src/main/java/net/zoda/api/command/wrapper/CommandManager.java
@@ -1,17 +1,24 @@
 package net.zoda.api.command.wrapper;
 
+import lombok.Getter;
+import net.zoda.api.command.ICommand;
 import net.zoda.api.command.argument.registry.ArgumentTypeRegistry;
+
+import java.lang.reflect.Method;
 
 
 public abstract class CommandManager<T> {
 
-    public CommandManager(Class<T> actorClass) {
+    @Getter private final Class<T> defaultActorClass;
 
+    public CommandManager(Class<T> actorClass) {
+        this.defaultActorClass = actorClass;
     }
 
     public void registerCustomArgumentTypes(ArgumentTypeRegistry argumentTypeRegistry) {
 
     }
 
-    public abstract Class<? extends T> actorClass();
+    public Class<? extends T> actorClassSubcommand(Method method) { return defaultActorClass; }
+    public Class<? extends T> actorClassClass(ICommand command) { return defaultActorClass; }
 }
