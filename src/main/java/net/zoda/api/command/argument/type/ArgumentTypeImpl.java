0cd9a8105dc0ea0451bd8bcadf08aa2a041b4ae3
novastosha <galliigotu@gmail.com>
2022-08-17 11:30:05 +0100

    Added a method to resolve T from a String array

--- src/main/java/net/zoda/api/command/argument/type/ArgumentTypeImpl.java
+++ src/main/java/net/zoda/api/command/argument/type/ArgumentTypeImpl.java
@@ -1,11 +1,9 @@
 package net.zoda.api.command.argument.type;
 
 import net.zoda.api.command.ICommand;
-import org.bukkit.command.CommandSender;
 
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Method;
-import java.util.List;
 import java.util.logging.Logger;
 
 public interface ArgumentTypeImpl<T, A extends Annotation> {
@@ -13,11 +11,13 @@ public interface ArgumentTypeImpl<T, A extends Annotation> {
     String stringify(T value, A annotation,
                      Method method, ICommand command);
 
-    int maximumArgs(CommandSender sender, A annotation,
+    T fromString(String[] args, A annotation, Method method, ICommand command);
+
+    int maximumArgs(A annotation,
                     Method method, ICommand command);
 
     default boolean verifyAnnotation(A annotation, Logger logger,
-                                     Method method, ICommand command) {
+                                     Method method, ICommand command,Class<?> parameterType) {
         return true;
     }
 
